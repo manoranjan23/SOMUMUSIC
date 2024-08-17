04be1710d7ab19e4bc77280320bd77f0286c507e
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-08-17 11:51:29 +0530

    Update Youtube.py

--- SomuX/platforms/Youtube.py
+++ SomuX/platforms/Youtube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from SYSTUM.utils.database import is_on_off
-from SYSTUM.utils.formatters import time_to_seconds
+from SomuX.utils.database import is_on_off
+from SomuX.utils.formatters import time_to_seconds
 
 
 
